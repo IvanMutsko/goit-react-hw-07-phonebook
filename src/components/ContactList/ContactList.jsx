9c101ade0fe7963817f8cab9117887c554fbdb35
Ivan Mutsko <imucko1996@gmail.com>
2023-05-23 19:38:58 +0300

    refactoring & add sort contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,36 +3,40 @@ import { useSelector } from 'react-redux';
 import { getFilter } from '../../redux/selectors';
 import { useGetContactsQuery } from 'redux/contactsSlice';
 import { ContactItem } from './ContactItem';
+import { sortArrOfObj } from 'utils/sortArrOfObj';
 
 const ContactList = () => {
   const filter = useSelector(getFilter);
   const { data: contacts, isLoading } = useGetContactsQuery();
 
   const filteringContactsList = () => {
-    const filteredContacts =
-      contacts?.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-      ) ?? [];
-
+    if (!contacts) return [];
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return filteredContacts;
   };
 
-  let filteredContactsData = filteringContactsList();
+  const filteredContactsData = filteringContactsList();
+  const sortedContactsData = sortArrOfObj(filteredContactsData);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-  return filteredContactsData.length === 0 ? (
-    <p>Sorry, but you don't have any contacts yet. Add your first contact.</p>
-  ) : (
+  if (sortedContactsData.length === 0) {
+    return (
+      <p>Sorry, but you don't have any contacts yet. Add your first contact.</p>
+    );
+  }
+
+  return (
     <List>
-      {!isLoading &&
-        filteredContactsData.map(({ id, name, phone }) => {
-          return (
-            <ListItem key={id}>
-              <ContactItem name={name} phone={phone} id={id} />
-            </ListItem>
-          );
-        })}
+      {sortedContactsData.map(({ id, name, phone }) => (
+        <ListItem key={id}>
+          <ContactItem name={name} phone={phone} id={id} />
+        </ListItem>
+      ))}
     </List>
   );
 };
